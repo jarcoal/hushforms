396f0374c57283d88c3836e3e2dea2892e8392ef
Jared Morse <jarcoal@gmail.com>
2017-12-27 14:20:29 -0500

    install/configure html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig],
 }
