a7c3fc47e9f4848fe3f9cb9e55a64f3e2d66f7fb
Jared Morse <jarcoal@gmail.com>
2017-12-27 15:12:38 -0500

    install bootstrap 4 beta and configure webpack to process scss files

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,27 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles SASS to CSS
+        }]
+      }
     ]
   },
   plugins: [HtmlWebpackPluginConfig],
